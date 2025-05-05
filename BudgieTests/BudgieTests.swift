c3e0f44029c109a833fa9ce38096a52e097c1457
Daniel <djszurek@crimson.ua.edu>
2025-05-05 11:56:36 -0500

    Initial Commit

---
+++ BudgieTests/BudgieTests.swift
@@ -0,0 +1,16 @@
+//
+//  BudgieTests.swift
+//  BudgieTests
+//
+//  Created by Daniel Szurek on 5/5/25.
+//
+
+import Testing
+
+struct BudgieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
